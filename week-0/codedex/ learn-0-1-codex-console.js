8f94df74801730ab91042d693c1aac93cc5f857d
Landon Umland <lumland@ewu.edu>
2026-01-11 00:44:46 +0000

    Add learn 0-1 Codedex exercises

---
+++ week-0/codedex/ learn-0-1-codex-console.js
@@ -0,0 +1,69 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: Landon Umland
+// Date: 1/10/26
+// ============================================
+
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// --------------------------------------------
+console.log("Hello internet!");
+// Add your custom greeting below:
+​
+console.log("Hello! I miss my dogs")
+​
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Your favorite food
+// --------------------------------------------
+​
+console.log("🍎 MY FAVORITE FOOD IS APPLES")
+​
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+
+console.log("   a   ")
+console.log("  b c  ")
+console.log(" d e f ")
+console.log("g h i j")
+console.log("   k   ")
+
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// --------------------------------------------
+
+// PB & J Recipe
+console.log("Ingredients")
+console.log("Bread")
+console.log("Chunky Peanut Butter")
+console.log("Raspberry Jelly")
+console.log("Steps")
+console.log("Step 1. Lightly Toast your bread of choice")
+console.log("Step 2. Spread your peanut butter on one slice of bread")
+console.log("Step 3. Spread your jelly on the other slice of bread")
+console.log("Step 4. Combine each slice together, spread side inwards")
+console.log("Step 5. Enjoy")
+// Secret Ingredient: Use brioche for best results
+
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+
+console.log("==================================")
+console.log("         McDONALD'S RECEIPT       ")
+console.log("----------------------------------")
+console.log("           Order Details          ")
+console.log("----------------------------------")
+console.log("Item           Quantity   Price   ")
+console.log("----------------------------------")
+console.log(" Crispy Chkn    1          7.00   ")
+console.log(" Fries          1          3.50   ")
+console.log(" Crml Frappe    1          5.00   ")
+console.log("----------------------------------")
+console.log("Total                      15.50  ")
+console.log("==================================")
