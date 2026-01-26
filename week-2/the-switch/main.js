c8071bf609b6f0d8cb293c6a419f9fe88d0b855b
Landon Umland <lumland@ewu.edu>
2026-01-26 03:29:47 +0000

    added functionality to the switch

--- week-2/the-switch/main.js
+++ week-2/the-switch/main.js
@@ -1,10 +1,11 @@
 // Step 1: Find the button
 // What selector goes here? Look at your HTML — what class is on the button?
-const toggle = document.querySelector('______');
+const toggle = document.querySelector('.toggle');
 
 // Step 2: Listen for clicks
 toggle.addEventListener('click', function() {
   // Step 3: Toggle the dark class on body
+  document.body.classList.toggle('dark');
   // What class are we toggling? Look at your CSS — what class changes the colors?
-  document.body.classList.toggle('______');
+  
 });
